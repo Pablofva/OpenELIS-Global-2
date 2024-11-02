b07dafaab4534f5fa60253ebb808975d7652c798
Pablofva <pablovzqz768@gmail.com>
2024-11-02 16:32:49 -0300

    SE TRABAJA SOBRE EL INFORME

--- src/main/java/org/openelisglobal/reports/action/implementation/PatientCILNSPClinical_vreduit.java
+++ src/main/java/org/openelisglobal/reports/action/implementation/PatientCILNSPClinical_vreduit.java
@@ -377,14 +377,6 @@ public class PatientCILNSPClinical_vreduit extends PatientReport implements IRep
             reportItem.setCompleteFlag(MessageUtil
                     .getMessage(sampleCompleteMap.get(reportItem.getAccessionNumber()) ? "report.status.complete"
                             : "report.status.partial"));
-            if (reportItem.isCorrectedResult()) {
-                if (reportItem.getNote() != null && reportItem.getNote().length() > 0) {
-                    reportItem.setNote(MessageUtil.getMessage("result.corrected") + "<br/>" + reportItem.getNote());
-                } else {
-                    reportItem.setNote(MessageUtil.getMessage("result.corrected"));
-                }
-            }
-
             reportItem
                     .setCorrectedResult(sampleCorrectedMap.get(reportItem.getAccessionNumber().split("_")[0]) != null);
         }
